1d442e0fe7900d55571b83bdaaf5ac5ca1576f99
Igor Moiseyenko <imoiseyenko93@gmail.com>
2014-11-30 16:50:40 +0300

    Cluster estimation base on Yager and Filev's procedure with default value of eps.

--- src/fp/lab1/core.clj
+++ src/fp/lab1/core.clj
@@ -8,6 +8,10 @@
 (def rA 2)
 (def rB (* 1.5 rA))
 (def alpha (/ 4 (* rA rA)))
+(def beta (/ 4 (* rB rB)))
+(def eps 0.55)
+(def greatEps 0.5)
+(def smallEps 0.15)
 
 "Lab 1 interface."
 (declare run-cluster-estimation)
@@ -66,12 +70,44 @@
           {}
           (keys dataPoints)))
 
+"Returns id of data point with mas potential."
+(defn- get-data-point-id-with-max-potential
+  [dataPointsPotentials]
+  (key (apply max-key val dataPointsPotentials)))
+
+"Revise potentials after one more cluster selected."
+(defn- revise-potentials
+  [clusterIds lastClusterPotentialId dataPointsPotentials distanceSquares]
+  (reduce (fn [revisedPotentials restPotentialId]
+            (assoc revisedPotentials
+                   restPotentialId
+                   (- (get dataPointsPotentials restPotentialId)
+                      (* (get dataPointsPotentials lastClusterPotentialId)
+                         (Math/pow Math/E (- (* beta (get-in distanceSquares [lastClusterPotentialId restPotentialId]))))))))
+          {}
+          (keys (apply dissoc dataPointsPotentials clusterIds))))
+
+(defn- get-clusters
+  [dataPoints dataPointsPotentials distanceSquares]
+  (loop [lastClusterId (get-data-point-id-with-max-potential dataPointsPotentials)
+         clusters { lastClusterId (get dataPointsPotentials lastClusterId) }
+         revisedPotentials (revise-potentials (keys clusters) lastClusterId dataPointsPotentials distanceSquares)]
+    (let [nextClusterId (get-data-point-id-with-max-potential revisedPotentials)]
+      (prn nextClusterId)
+      (prn (get revisedPotentials nextClusterId))
+      (if (< (get revisedPotentials nextClusterId) (* eps (get dataPointsPotentials lastClusterId)))
+        clusters
+        (let [newClusters (assoc clusters nextClusterId (get revisedPotentials nextClusterId))
+              newRevisedPotentials (revise-potentials (keys newClusters) nextClusterId dataPointsPotentials distanceSquares)]
+          (recur nextClusterId newClusters newRevisedPotentials))))))
+
 (defn run-cluster-estimation
   []
   (let [dataLines (util/read-file dataFilePath)
         dataPoints (getDataPoints dataLines)
         distanceSquares (squares-of-the-distances dataPoints)
-        dataPointsPotentials (data-points-potentials dataPoints distanceSquares)]
+        dataPointsPotentials (data-points-potentials dataPoints distanceSquares)
+        clusters (get-clusters dataPoints dataPointsPotentials distanceSquares)]
     (prn dataPoints)
-    (prn distanceSquares)
-    (prn dataPointsPotentials)))
+    (prn dataPointsPotentials)
+    (prn clusters)))
