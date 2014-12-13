a13d1b1dc18709bc2cc47891d28855d525f6d129
Igor Moiseyenko <imoiseyenko93@gmail.com>
2014-12-13 18:04:22 +0300

    Depth-first algorithm for traversing URL's graph. pmap function used to apply URL parsing functions in parallel. Pretty output in the file.

--- src/fp/lab2/core.clj
+++ src/fp/lab2/core.clj
@@ -1,19 +1,53 @@
 (ns fp.lab2.core
   (:require [fp.util :as util]
-            [fp.lab2.urls :as links])
+            [fp.lab2.urls :as urls])
   (:require [clj-http.client :as client]))
 
 (declare run-url-analyzer)
+(declare handle-urls)
 
-(def htmlPath "./resources/lab2/lab2.html")
+"Status codes to handle."
+(def ok 200)
+(def notFound 404)
+(def redirects [300 301 302 303 307])
 
+"Initial parameters."
+(def linksPath "./resources/lab2/links")
+(def depth 2)
+
+"Path to the output."
+(def outputPath "./resources/lab2/output")
+
+"Handle external url.
+ Depth-first algorithm for traversing URL's graph.
+ Recall function to handle urls with decrementet depth."
 (defn- handle-url
-  [url]
-  )
+  [externalUrl depth]
+  (let [response (client/get externalUrl { :throw-exceptions false })
+        statusCode (:status response)]
+    (cond
+      (= statusCode ok) (let [externalSubUrls (urls/parse-external-urls (:body response))]
+                          { :numOfLinks (count externalSubUrls)
+                            :links (if (> depth 0)
+                                     (handle-urls externalSubUrls (- depth 1))
+                                     []) })
+      (= statusCode notFound) "Bad"
+      (contains? redirects statusCode) (let [traceRedirects (:trace-redirects response)]
+                                         (str "Redirect to " (peek traceRedirects))))))
 
+"Handle external urls with specified depth."
+(defn- handle-urls
+  [externalUrls depth]
+  (pmap (fn [externalUrl]
+          { externalUrl (handle-url externalUrl depth) })
+        externalUrls))
+
+"Run urls analyzer."
 (defn run-url-analyzer
   []
-  (let [testHtml (slurp htmlPath)
-        urls (links/parse-external-urls testHtml)]
+  (let [urls (util/read-file linksPath)
+        urlsInfo (handle-urls urls depth)]
     (prn 'Result)
-    (prn urls)))
+    (prn urls)
+    (prn urlsInfo)
+    (util/write-dataset-to-file outputPath urlsInfo)))
--- src/fp/util.clj
+++ src/fp/util.clj
@@ -1,8 +1,16 @@
 (ns fp.util
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pprint]))
 
 "Read file line by line and return them as a vector."
 (defn read-file
   [filePath]
   (with-open [reader (io/reader filePath)]
     (reduce conj [] (line-seq reader))))
+
+"Pretty write-to-file function."
+(defn write-dataset-to-file
+  [outputPath dataset]
+  (with-open [writer (io/writer outputPath)]
+    (binding [*out* writer]
+      (pprint/write dataset))))
